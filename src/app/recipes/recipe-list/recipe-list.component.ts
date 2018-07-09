06eeb3aeba8430a3301f3d1bb9b43f4acff0e30e
Carlos Albuquerque Carneiro <carlos.carneiro@cedrofinances.com.br>
2018-07-09 10:42:09 -0300

    Section 3 - Complete

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Test Recipe", "Test", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"),
+    new Recipe("Test Recipe 2", "Test 2", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"),
+    new Recipe("Test Recipe 3", "Test 3", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"),
+    new Recipe("Test Recipe 4", "Test 4", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg"),
+    new Recipe("Test Recipe 5", "Test 5", "https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg")
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+
+    //automatically create two properties and assign values
+    constructor(public name: string, public amount: number) {
+    }
+}
