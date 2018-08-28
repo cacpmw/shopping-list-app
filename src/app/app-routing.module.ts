ab5443a19d6ba5b1652dfae2f0d129d7d4390a1b
Carlos Albuquerque Carneiro <carlos.carneiro@cedrotech.com>
2018-08-28 16:31:41 -0300

    Section 22, Lecture 296

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './core/home/home.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
-  {path:'shopping-list',loadChildren:'./shopping-list/shopping-list.module#ShoppingListModule'}
+  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },//lazy loading
+  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule' }//lazy loading
 
 
 ];
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
 
   ],
   declarations: [],
